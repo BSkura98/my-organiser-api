9d236e8b2563b642025b3eb20315f60a81ed2641
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2023-08-24 18:55:06 +0200

    Add typescript to the project

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import fs from "fs";
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
-fs;
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
