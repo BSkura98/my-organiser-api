a4eeb41544eb12d101cb12cc410373defadf27b3
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2023-08-25 17:46:03 +0200

    Create GET /posts (returning fake data) (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
+import { createApp } from "./app.js";
+
 dotenv.config();
 
-const app: Express = express();
+const app = createApp();
 const port = process.env.PORT || 8080;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
