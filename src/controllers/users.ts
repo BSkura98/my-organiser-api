1a5aab6bb9bcf5c593c38a7fca1509684f72cd57
Bartłomiej Skura <bartek33s@wp.pl>
2023-08-29 16:47:02 +0200

    Add login endpoint

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 
 interface User {
-  username: string;
+  name: string;
   password: string;
 }
 
@@ -15,7 +15,7 @@ const getUsers = async (req: Request, res: Response) => {
 const addUser = async (req: Request, res: Response) => {
   try {
     const hashedPassword = await hash(req.body.password, 10);
-    const user = { username: req.body.username, password: hashedPassword };
+    const user = { name: req.body.name, password: hashedPassword };
     users.push(user);
     res.status(201).send();
   } catch {
@@ -23,4 +23,20 @@ const addUser = async (req: Request, res: Response) => {
   }
 };
 
-export { getUsers, addUser };
+const login = async (req: Request, res: Response) => {
+  const user = users.find((user) => user.name === req.body.name);
+  if (user == null) {
+    return res.status(404).send("User not found");
+  }
+  try {
+    if (await compare(req.body.password, user.password)) {
+      res.status(200).send("Successfully logged in");
+    } else {
+      res.status(401).send("Incorrect password");
+    }
+  } catch {
+    res.status(500).send();
+  }
+};
+
+export { getUsers, addUser, login };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { addUser, getUsers } from "../controllers/users.js";
+import { addUser, getUsers, login } from "../controllers/users.js";
 
 const router = express.Router();
 
 router.route("/").get(getUsers).post(addUser);
+router.route("/login").post(login);
 
 export default router;
