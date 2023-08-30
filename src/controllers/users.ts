230a2766c2420acd67e19639f698491d92a6490c
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2023-08-30 13:52:24 +0200

    Add login endpoint (#5)
    * Create GET and POST /users
    
    * Hash password
    
    * Add login endpoint
    
    * Generate access token

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
+import jwt from "jsonwebtoken";
 
 interface User {
-  username: string;
+  name: string;
   password: string;
 }
 
@@ -15,7 +16,7 @@ const getUsers = async (req: Request, res: Response) => {
 const addUser = async (req: Request, res: Response) => {
   try {
     const hashedPassword = await hash(req.body.password, 10);
-    const user = { username: req.body.username, password: hashedPassword };
+    const user = { name: req.body.name, password: hashedPassword };
     users.push(user);
     res.status(201).send();
   } catch {
@@ -23,4 +24,23 @@ const addUser = async (req: Request, res: Response) => {
   }
 };
 
-export { getUsers, addUser };
+const login = async (req: Request, res: Response) => {
+  const user = users.find((user) => user.name === req.body.name);
+  if (user == null) {
+    return res.status(404).send("User not found");
+  }
+  try {
+    if (await compare(req.body.password, user.password)) {
+      const userData = { name: user.name };
+
+      const accessToken = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET);
+      res.status(200).json({ accessToken: accessToken });
+    } else {
+      res.status(401).send("Incorrect password");
+    }
+  } catch {
+    res.status(500).send();
+  }
+};
+
+export { getUsers, addUser, login };
