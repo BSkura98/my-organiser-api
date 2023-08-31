bea1fa54351c77a6f0791085fee50f8cb17aedbe
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2023-08-31 17:31:21 +0200

    Authenticate token (#6)
    * Create GET and POST /users
    
    * Hash password
    
    * Add login endpoint
    
    * Generate access token
    
    * Authenticate token

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,15 @@ import express, { Express } from "express";
 
 import posts from "./routes/posts.js";
 import users from "./routes/users.js";
+import { authenticateToken } from "./middleware/authenticateToken.js";
 
 export const createApp = () => {
   const app: Express = express();
 
   app.use(express.json({ limit: "50mb" }));
 
-  app.use("/posts", posts);
   app.use("/users", users);
+  app.use("/posts", authenticateToken, posts);
 
   return app;
 };
