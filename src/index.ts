ebc113e08853c98e18f2341bde4c00cb81011e53
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2024-02-12 09:50:20 +0100

    Add databaseInitialized variable in index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,18 @@ dotenv.config();
 const app = createApp();
 const port = process.env.PORT || 8080;
 
-AppDataSource.initialize()
+const databaseInitialized = await AppDataSource.initialize()
   .then(() => {
     console.log("Data Source has been initialized!");
-    app.listen(port, () => {
-      console.log(`Server is running at http://localhost:${port}`);
-    });
+    return true;
   })
   .catch((err) => {
     console.error("Error during Data Source initialization", err);
+    return false;
   });
+
+if (databaseInitialized) {
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
+}
